05c802a20e3d007c53d945bb0cfaa756f4e8758e
Ryan Kumsher <RKumsher@gmail.com>
2017-02-04 17:51:23 -0500

    Add RandomCollectionUtils with methods to generate random lists

---
+++ src/main/java/kumsher/ryan/collection/RandomCollectionUtils.java
@@ -0,0 +1,76 @@
+package kumsher.ryan.collection;
+
+import static com.google.common.base.Preconditions.*;
+import static com.google.common.collect.Iterables.isEmpty;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.google.common.collect.ContiguousSet;
+import com.google.common.collect.DiscreteDomain;
+import com.google.common.collect.Range;
+
+/** Utility library to return random collections. */
+public class RandomCollectionUtils {
+
+  /**
+   * Returns a random list filled with the given elements.
+   *
+   * @param elements elements to fill list from
+   * @param size range that the size of the collection will be randomly chosen from
+   * @return random list filled with the given elements
+   * @throws IllegalArgumentException if the elements to fill list from is empty or if the size
+   *     range contains negative integers
+   */
+  public static <T> List<T> randomListFrom(Iterable<T> elements, Range<Integer> size) {
+    checkArgument(!isEmpty(elements), "Elements to populate random list from must not be empty");
+    return randomListFrom(() -> IterableUtils.randomFrom(elements), size);
+  }
+
+  /**
+   * Returns a random list filled with the given elements.
+   *
+   * @param elements elements to fill list from
+   * @param size of the random collection to return
+   * @return random list filled with the given elements
+   * @throws IllegalArgumentException if the elements to fill list from is empty or if the size is
+   *     negative
+   */
+  public static <T> List<T> randomListFrom(Iterable<T> elements, int size) {
+    checkArgument(!isEmpty(elements), "Elements to populate random list from must not be empty");
+    return randomListFrom(() -> IterableUtils.randomFrom(elements), size);
+  }
+
+  /**
+   * Returns a random list filled from the given element supplier.
+   *
+   * @param elementSupplier element supplier to fill list from
+   * @param size range that the size of the collection will be randomly chosen from
+   * @return random list filled with the given elements
+   * @throws IllegalArgumentException if the size range contains negative integers
+   */
+  public static <T> List<T> randomListFrom(Supplier<T> elementSupplier, Range<Integer> size) {
+    checkArgument(
+        size.hasLowerBound() && size.lowerEndpoint() >= 0,
+        "Size range must consist of only positive integers");
+    Set<Integer> rangeSet = ContiguousSet.create(size, DiscreteDomain.integers());
+    int limit = IterableUtils.randomFrom(rangeSet);
+    return randomListFrom(elementSupplier, limit);
+  }
+
+  /**
+   * Returns a random list filled from the given element supplier.
+   *
+   * @param elementSupplier element supplier to fill list from
+   * @param size of the random collection to return
+   * @return random list filled with the given elements
+   * @throws IllegalArgumentException if the size is negative
+   */
+  public static <T> List<T> randomListFrom(Supplier<T> elementSupplier, int size) {
+    checkArgument(size >= 0, "Size must be greater than or equal to zero");
+    return Stream.generate(elementSupplier).limit(size).collect(Collectors.toList());
+  }
+}
---
+++ src/test/java/kumsher/ryan/collection/RandomCollectionUtilsTest.java
@@ -0,0 +1,89 @@
+package kumsher.ryan.collection;
+
+import static kumsher.ryan.collection.RandomCollectionUtils.randomListFrom;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isIn;
+import static org.junit.Assert.*;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Range;
+
+public class RandomCollectionUtilsTest {
+
+  @Test
+  public void randomListFrom_WithConstantSupplier_ReturnsListFilledWithConstant() {
+    Object onlyElement = new Object();
+    List<Object> list = randomListFrom(() -> onlyElement, Range.singleton(2));
+    assertThat(list, is(Lists.newArrayList(onlyElement, onlyElement)));
+  }
+
+  @Test
+  public void randomListFrom_WithZeroSize_ReturnsEmptyList() {
+    Object onlyElement = new Object();
+    List<Object> list = randomListFrom(() -> onlyElement, Range.singleton(0));
+    assertThat(list, empty());
+  }
+
+  @Test
+  public void randomListFrom_WithSizeRangeContainsNegative_ThrowsIllegalArgumentException() {
+    try {
+      randomListFrom(Object::new, Range.closedOpen(-1, 1));
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Size range must consist of only positive integers"));
+    }
+  }
+
+  @Test
+  public void randomListFrom_WithNegativeSize_ThrowsIllegalArgumentException() {
+    try {
+      randomListFrom(Object::new, -1);
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Size must be greater than or equal to zero"));
+    }
+  }
+
+  @Test
+  public void randomListFrom_WithUnboundedLowerHowMany_ThrowsIllegalArgumentException() {
+    try {
+      randomListFrom(Object::new, Range.all());
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Size range must consist of only positive integers"));
+    }
+  }
+
+  @Test
+  public void randomListFrom_WithEmptyCollection_ThrowsIllegalArgumentException() {
+    try {
+      randomListFrom(Collections.emptyList(), Range.singleton(2));
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Elements to populate random list from must not be empty"));
+    }
+  }
+
+  @Test
+  public void randomListFrom_WithSingletonCollection_ReturnsListFilledWithOnlyElement() {
+    Object onlyElement = new Object();
+    List<Object> list = randomListFrom(Collections.singleton(onlyElement), Range.singleton(2));
+    assertThat(list, is(Lists.newArrayList(onlyElement, onlyElement)));
+  }
+
+  @Test
+  public void randomListFrom_WithCollection_ReturnsListFilledWithElementsFromCollection() {
+    List<Object> from = Lists.newArrayList(new Object(), new Object());
+    List<Object> list = randomListFrom(from, 10);
+    assertThat(list, hasSize(10));
+    assertThat(list, everyItem(isIn(from)));
+  }
+}
