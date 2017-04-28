e835ce295583ecb99fcfbc9daa27ae9e5f526fbe
Ryan Kumsher <RKumsher@gmail.com>
2017-04-27 20:52:19 -0400

    Add methods to generate random ZonedDateTimes to RandomDateUtils

--- src/main/java/kumsher/ryan/date/RandomDateUtils.java
+++ src/main/java/kumsher/ryan/date/RandomDateUtils.java
@@ -5,6 +5,7 @@ import static com.google.common.base.Preconditions.*;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
 import org.apache.commons.lang3.RandomUtils;
@@ -15,29 +16,104 @@ import org.apache.commons.lang3.RandomUtils;
  */
 public class RandomDateUtils {
 
-  private static final ZoneId DEFAULT_ZONE = ZoneId.systemDefault();
-  public static final Instant MIN_INSTANT = Instant.ofEpochMilli(0);
+  static final ZoneId DEFAULT_ZONE = ZoneId.systemDefault();
+  static final Instant MIN_INSTANT = Instant.ofEpochMilli(0);
   /* Limiting to just 4-digit years */
-  public static final Instant MAX_INSTANT =
+  static final Instant MAX_INSTANT =
       Instant.ofEpochMilli(
           LocalDate.of(10000, 1, 1).atStartOfDay(DEFAULT_ZONE).toInstant().toEpochMilli());
 
   /**
-   * Returns a random instant between {@link RandomDateUtils#MIN_INSTANT} and {@link
+   * Returns a random {@link ZonedDateTime} between {@link RandomDateUtils#MIN_INSTANT} and {@link
    * RandomDateUtils#MAX_INSTANT}.
    *
-   * @return the random instant
+   * @return the random {@link ZonedDateTime}
+   */
+  public static ZonedDateTime randomZonedDateTime() {
+    return ZonedDateTime.ofInstant(randomInstant(), DEFAULT_ZONE);
+  }
+
+  /**
+   * Returns a random {@link ZonedDateTime} within the specified range.
+   *
+   * @param startInclusive the earliest {@link ZonedDateTime} that can be returned
+   * @param endExclusive the upper bound (not included)
+   * @return the random {@link ZonedDateTime}
+   * @throws IllegalArgumentException if startInclusive or endExclusive are null or if endExclusive
+   *     is earlier than startInclusive
+   */
+  public static ZonedDateTime randomZonedDateTime(
+      ZonedDateTime startInclusive, ZonedDateTime endExclusive) {
+    checkArgument(startInclusive != null, "Start must be non-null");
+    checkArgument(endExclusive != null, "End must be non-null");
+    Instant instant = randomInstant(startInclusive.toInstant(), endExclusive.toInstant());
+    return ZonedDateTime.ofInstant(instant, DEFAULT_ZONE);
+  }
+
+  /**
+   * Returns a random {@link ZonedDateTime} that is after the current system clock.
+   *
+   * @return the random {@link ZonedDateTime}
+   */
+  public static ZonedDateTime randomFutureZonedDateTime() {
+    Instant instant = randomInstant(Instant.now().plus(1, ChronoUnit.MILLIS), MAX_INSTANT);
+    return ZonedDateTime.ofInstant(instant, DEFAULT_ZONE);
+  }
+
+  /**
+   * Returns a random {@link ZonedDateTime} that is after the given {@link ZonedDateTime}.
+   *
+   * @param after the value that returned {@link ZonedDateTime} must be after
+   * @return the random {@link ZonedDateTime}
+   * @throws IllegalArgumentException if after is null or if after is equal to or after {@link
+   *     RandomDateUtils#MAX_INSTANT}
+   */
+  public static ZonedDateTime randomFutureZonedDateTime(ZonedDateTime after) {
+    checkArgument(after != null, "After must be non-null");
+    Instant instant = randomFutureInstant(after.toInstant());
+    return ZonedDateTime.ofInstant(instant, DEFAULT_ZONE);
+  }
+
+  /**
+   * Returns a random {@link ZonedDateTime} that is before the current system clock.
+   *
+   * @return the random {@link ZonedDateTime}
+   */
+  public static ZonedDateTime randomPastZonedDateTime() {
+    Instant instant = randomInstant(MIN_INSTANT, Instant.now());
+    return ZonedDateTime.ofInstant(instant, DEFAULT_ZONE);
+  }
+
+  /**
+   * Returns a random {@link ZonedDateTime} that is before the given {@link ZonedDateTime}.
+   *
+   * @param before the value that returned {@link ZonedDateTime} must be before
+   * @return the random {@link ZonedDateTime}
+   * @throws IllegalArgumentException if before is null or if before is equal to or before {@link
+   *     RandomDateUtils#MIN_INSTANT}
+   */
+  public static ZonedDateTime randomPastZonedDateTime(ZonedDateTime before) {
+    checkArgument(before != null, "Before must be non-null");
+    Instant instant = randomPastInstant(before.toInstant());
+    return ZonedDateTime.ofInstant(instant, DEFAULT_ZONE);
+  }
+
+  /**
+   * Returns a random {@link Instant} between {@link RandomDateUtils#MIN_INSTANT} and {@link
+   * RandomDateUtils#MAX_INSTANT}.
+   *
+   * @return the random {@link Instant}
    */
   public static Instant randomInstant() {
     return randomInstant(MIN_INSTANT, MAX_INSTANT);
   }
 
   /**
-   * Returns a random instant within the specified range.
+   * Returns a random {@link Instant} within the specified range.
    *
-   * @param startInclusive the earliest instant that can be returned
+   * @param startInclusive the earliest {@link Instant} that can be returned
    * @param endExclusive the upper bound (not included)
-   * @return the random instant
+   * @return the random {@link Instant}
    * @throws IllegalArgumentException if startInclusive or endExclusive are null or if endExclusive
    *     is earlier than startInclusive
    */
@@ -50,19 +126,19 @@ public class RandomDateUtils {
   }
 
   /**
-   * Returns a random instant that is after the current system clock.
+   * Returns a random {@link Instant} that is after the current system clock.
    *
-   * @return the random instant
+   * @return the random {@link Instant}
    */
   public static Instant randomFutureInstant() {
     return randomInstant(Instant.now().plus(1, ChronoUnit.MILLIS), MAX_INSTANT);
   }
 
   /**
-   * Returns a random instant that is after the given instant.
+   * Returns a random {@link Instant} that is after the given {@link Instant}.
    *
-   * @param after the value that returned instant must be after
-   * @return the random instant
+   * @param after the value that returned {@link Instant} must be after
+   * @return the random {@link Instant}
    * @throws IllegalArgumentException if after is null or if after is equal to or after {@link
    *     RandomDateUtils#MAX_INSTANT}
    */
@@ -73,19 +149,19 @@ public class RandomDateUtils {
   }
 
   /**
-   * Returns a random instant that is before the current system clock.
+   * Returns a random {@link Instant} that is before the current system clock.
    *
-   * @return the random instant
+   * @return the random {@link Instant}
    */
   public static Instant randomPastInstant() {
     return randomInstant(MIN_INSTANT, Instant.now());
   }
 
   /**
-   * Returns a random instant that is before the given instant.
+   * Returns a random {@link Instant} that is before the given {@link Instant}.
    *
-   * @param before the value that returned instant must be before
-   * @return the random instant
+   * @param before the value that returned {@link Instant} must be before
+   * @return the random {@link Instant}
    * @throws IllegalArgumentException if before is null or if before is equal to or before {@link
    *     RandomDateUtils#MIN_INSTANT}
    */
--- src/test/java/kumsher/ryan/date/RandomDateUtilsTest.java
+++ src/test/java/kumsher/ryan/date/RandomDateUtilsTest.java
@@ -1,19 +1,157 @@
 package kumsher.ryan.date;
 
+import static kumsher.ryan.date.RandomDateUtils.DEFAULT_ZONE;
 import static kumsher.ryan.date.RandomDateUtils.MAX_INSTANT;
 import static kumsher.ryan.date.RandomDateUtils.MIN_INSTANT;
 import static kumsher.ryan.date.RandomDateUtils.randomFutureInstant;
+import static kumsher.ryan.date.RandomDateUtils.randomFutureZonedDateTime;
 import static kumsher.ryan.date.RandomDateUtils.randomInstant;
 import static kumsher.ryan.date.RandomDateUtils.randomPastInstant;
+import static kumsher.ryan.date.RandomDateUtils.randomPastZonedDateTime;
+import static kumsher.ryan.date.RandomDateUtils.randomZonedDateTime;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 import java.time.Instant;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
 import org.junit.Test;
 
 public class RandomDateUtilsTest {
+
+  private static final ZonedDateTime MIN_ZONED_DATE_TIME =
+      ZonedDateTime.ofInstant(MIN_INSTANT, DEFAULT_ZONE);
+  private static final ZonedDateTime MAX_ZONED_DATE_TIME =
+      ZonedDateTime.ofInstant(MAX_INSTANT, DEFAULT_ZONE);
+
+  @Test
+  public void randomZonedDateTime_ReturnsZonedDateTimeBetweenMinAndMaxInstants() {
+    ZonedDateTime randomZonedDateTime = randomZonedDateTime();
+    assertTrue(
+        randomZonedDateTime.isAfter(MIN_ZONED_DATE_TIME)
+            || randomZonedDateTime.equals(MIN_ZONED_DATE_TIME));
+    assertTrue(randomZonedDateTime.isBefore(MAX_ZONED_DATE_TIME));
+  }
+
+  @Test
+  public void randomZonedDateTime_ReturnsZonedDateTimeBetweenGivenZonedDateTimes() {
+    ZonedDateTime start = ZonedDateTime.now().minus(1, ChronoUnit.DAYS);
+    ZonedDateTime end = ZonedDateTime.now().plus(1, ChronoUnit.DAYS);
+
+    ZonedDateTime zonedDateTime = randomZonedDateTime(start, end);
+    assertTrue(zonedDateTime.isAfter(start) || zonedDateTime.equals(start));
+    assertTrue(zonedDateTime.isBefore(end));
+  }
+
+  @Test
+  public void randomZonedDateTime_WithZonedDateTimesOneMillisecondApart_ReturnsStart() {
+    ZonedDateTime start = ZonedDateTime.now();
+    ZonedDateTime end = start.plus(1, ChronoUnit.MILLIS);
+    assertThat(randomZonedDateTime(start, end), is(start));
+  }
+
+  @Test
+  public void randomZonedDateTime_WithEqualZonedDateTimes_ReturnsStartZonedDateTime() {
+    ZonedDateTime zonedDateTime = ZonedDateTime.now();
+    assertThat(randomZonedDateTime(zonedDateTime, zonedDateTime), is(zonedDateTime));
+  }
+
+  @Test
+  public void randomZonedDateTime_WithNullEndZonedDateTime_ThrowsIllegalArgumentException() {
+    try {
+      randomZonedDateTime(ZonedDateTime.now(), null);
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("End must be non-null"));
+    }
+  }
+
+  @Test
+  public void randomZonedDateTime_WithNullStartZonedDateTime_ThrowsIllegalArgumentException() {
+    try {
+      randomZonedDateTime(null, ZonedDateTime.now());
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Start must be non-null"));
+    }
+  }
+
+  @Test
+  public void randomZonedDateTime_WithStartAfterEndZonedDateTime_ThrowsIllegalArgumentException() {
+    ZonedDateTime start = ZonedDateTime.now().plus(1, ChronoUnit.DAYS);
+    ZonedDateTime end = ZonedDateTime.now().minus(1, ChronoUnit.DAYS);
+    try {
+      randomZonedDateTime(start, end);
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("End must come on or after start"));
+    }
+  }
+
+  @Test
+  public void randomFutureZonedDateTime_WithAfterGiven_ReturnsZonedDateTimeAfterGiven() {
+    ZonedDateTime after = ZonedDateTime.now();
+    assertThat(randomFutureZonedDateTime(after).isAfter(after), is(true));
+  }
+
+  @Test
+  public void randomFutureZonedDateTime_WithMaxZonedDateTime_ThrowsIllegalArgumentException() {
+    try {
+      randomFutureZonedDateTime(MAX_ZONED_DATE_TIME);
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Cannot produce date after " + MAX_INSTANT));
+    }
+  }
+
+  @Test
+  public void randomFutureZonedDateTime_WithNullZonedDateTime_ThrowsIllegalArgumentException() {
+    try {
+      randomFutureZonedDateTime(null);
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("After must be non-null"));
+    }
+  }
+
+  @Test
+  public void randomFutureZonedDateTime_ReturnsZonedDateTimeAfterCurrentSystemClock() {
+    ZonedDateTime now = ZonedDateTime.now();
+    assertThat(randomFutureZonedDateTime().isAfter(now), is(true));
+  }
+
+  @Test
+  public void randomPastZonedDateTime_WithBeforeGiven_ReturnsZonedDateTimeBeforeGiven() {
+    ZonedDateTime before = ZonedDateTime.now();
+    assertThat(randomPastZonedDateTime(before).isBefore(before), is(true));
+  }
+
+  @Test
+  public void randomPastZonedDateTime_WithMinZonedDateTime_ThrowsIllegalArgumentException() {
+    try {
+      randomPastZonedDateTime(MIN_ZONED_DATE_TIME);
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Cannot produce date before " + MIN_INSTANT));
+    }
+  }
+
+  @Test
+  public void randomPastZonedDateTime_WithNullZonedDateTime_ThrowsIllegalArgumentException() {
+    try {
+      randomPastZonedDateTime(null);
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Before must be non-null"));
+    }
+  }
+
+  @Test
+  public void randomPastZonedDateTime_ReturnsZonedDateTimeBeforeCurrentSystemClock() {
+    ZonedDateTime now = ZonedDateTime.now();
+    assertThat(randomPastZonedDateTime().isBefore(now), is(true));
+  }
 
   @Test
   public void randomInstant_ReturnsInstantBetweenMinAndMaxInstants() {
@@ -23,7 +161,7 @@ public class RandomDateUtilsTest {
   }
 
   @Test
-  public void randomInstant_ReturnsInstantBetweenGivenInstant() {
+  public void randomInstant_ReturnsInstantBetweenGivenInstants() {
     Instant start = Instant.now().minus(1, ChronoUnit.DAYS);
     Instant end = Instant.now().plus(1, ChronoUnit.DAYS);
 
@@ -33,14 +171,14 @@ public class RandomDateUtilsTest {
   }
 
   @Test
-  public void randomInstant_WithInstantsOneMillisecondApart_ReturnsStartInstance() {
+  public void randomInstant_WithInstantsOneMillisecondApart_ReturnsStart() {
     Instant start = Instant.now();
     Instant end = start.plus(1, ChronoUnit.MILLIS);
     assertThat(randomInstant(start, end), is(start));
   }
 
   @Test
-  public void randomInstant_WithEqualInstants_ReturnsStartInstant() {
+  public void randomInstant_WithEqualInstants_ReturnsStart() {
     Instant instant = Instant.now();
     assertThat(randomInstant(instant, instant), is(instant));
   }
@@ -78,7 +216,7 @@ public class RandomDateUtilsTest {
   }
 
   @Test
-  public void randomFutureInstant_WithAfterGiven_ReturnsInstantAfterGivenInstant() {
+  public void randomFutureInstant_WithAfterGiven_ReturnsInstantAfterGiven() {
     Instant after = Instant.now();
     assertThat(randomFutureInstant(after).isAfter(after), is(true));
   }
@@ -86,7 +224,7 @@ public class RandomDateUtilsTest {
   @Test
   public void randomFutureInstant_WithMaxInstant_ThrowsIllegalArgumentException() {
     try {
-      RandomDateUtils.randomFutureInstant(MAX_INSTANT);
+      randomFutureInstant(MAX_INSTANT);
       fail("Should have thrown an IllegalArgumentException");
     } catch (IllegalArgumentException ex) {
       assertThat(ex.getMessage(), is("Cannot produce date after " + MAX_INSTANT));
@@ -110,13 +248,13 @@ public class RandomDateUtilsTest {
   }
 
   @Test
-  public void randomPastInstant_WithBeforeGiven_ReturnsInstantBeforeGivenInstant() {
+  public void randomPastInstant_WithBeforeGiven_ReturnsInstantBeforeGiven() {
     Instant before = Instant.now();
     assertThat(randomPastInstant(before).isBefore(before), is(true));
   }
 
   @Test
-  public void randomPastDate_WithMinInstant_ThrowsIllegalArgumentException() {
+  public void randomPastInstant_WithMinInstant_ThrowsIllegalArgumentException() {
     try {
       randomPastInstant(MIN_INSTANT);
       fail("Should have thrown an IllegalArgumentException");
