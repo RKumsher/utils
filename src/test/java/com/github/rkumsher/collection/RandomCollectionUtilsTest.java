6b703b4960fd0c74134901a82387a801472f3795
Ryan Kumsher <RKumsher@gmail.com>
2017-08-19 16:06:14 -0400

    Add randomSetFrom methods to RandomCollectionUtils
    Fixes #1

--- src/test/java/com/github/rkumsher/collection/RandomCollectionUtilsTest.java
+++ src/test/java/com/github/rkumsher/collection/RandomCollectionUtilsTest.java
@@ -1,28 +1,112 @@
 package com.github.rkumsher.collection;
 
 import static com.github.rkumsher.collection.RandomCollectionUtils.randomListFrom;
+import static com.github.rkumsher.collection.RandomCollectionUtils.randomSetFrom;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isIn;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.Assert.*;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Range;
+import com.google.common.collect.Sets;
 
 public class RandomCollectionUtilsTest {
+
+  @Test
+  public void randomSetFrom_WithConstantSupplier_ReturnsSetWithJustConstant() {
+    Object onlyElement = new Object();
+    Set<Object> set = randomSetFrom(() -> onlyElement, Range.singleton(2));
+    assertThat(set, contains(onlyElement));
+  }
+
+  @Test
+  public void randomSetFrom_WithZeroSize_ReturnsEmptySet() {
+    Object onlyElement = new Object();
+    Set<Object> set = randomSetFrom(() -> onlyElement, Range.singleton(0));
+    assertThat(set, empty());
+  }
+
+  @Test
+  public void randomSetFrom_WithSizeRangeContainsNegative_ThrowsIllegalArgumentException() {
+    try {
+      randomSetFrom(Object::new, Range.closedOpen(-1, 1));
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Size range must consist of only positive integers"));
+    }
+  }
+
+  @Test
+  public void randomSetFrom_WithNegativeSize_ThrowsIllegalArgumentException() {
+    try {
+      randomSetFrom(Object::new, -1);
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Size must be greater than or equal to zero"));
+    }
+  }
+
+  @Test
+  public void randomSetFrom_WithUnboundedLowerHowMany_ThrowsIllegalArgumentException() {
+    try {
+      randomSetFrom(Object::new, Range.all());
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Size range must consist of only positive integers"));
+    }
+  }
+
+  @Test
+  public void randomSetFrom_WithEmptyCollectionWithSize_ThrowsIllegalArgumentException() {
+    try {
+      randomSetFrom(Collections.emptySet(), 2);
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Elements to populate from must not be empty"));
+    }
+  }
+
+  @Test
+  public void randomSetFrom_WithEmptyCollectionWithRange_ThrowsIllegalArgumentException() {
+    try {
+      randomSetFrom(Collections.emptySet(), Range.singleton(2));
+      fail("Should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      assertThat(ex.getMessage(), is("Elements to populate from must not be empty"));
+    }
+  }
+
+  @Test
+  public void randomSetFrom_WithSingletonCollection_ReturnsSetFilledWithOnlyElement() {
+    Object onlyElement = new Object();
+    Set<Object> set = randomSetFrom(Collections.singleton(onlyElement), Range.singleton(2));
+    assertThat(set, contains(onlyElement));
+  }
+
+  @Test
+  public void randomSetFrom_WithCollection_ReturnsSetFilledWithElementsFromCollection() {
+    Set<Object> from = Sets.newHashSet(new Object(), new Object());
+    Set<Object> set = randomSetFrom(from, 10);
+    assertThat(set, hasSize(lessThanOrEqualTo(2)));
+    assertThat(set, everyItem(isIn(from)));
+  }
 
   @Test
   public void randomListFrom_WithConstantSupplier_ReturnsListFilledWithConstant() {
     Object onlyElement = new Object();
     List<Object> list = randomListFrom(() -> onlyElement, Range.singleton(2));
-    assertThat(list, is(Lists.newArrayList(onlyElement, onlyElement)));
+    assertThat(list, contains(onlyElement, onlyElement));
   }
 
   @Test
@@ -68,7 +152,7 @@ public class RandomCollectionUtilsTest {
       randomListFrom(Collections.emptyList(), 2);
       fail("Should have thrown an IllegalArgumentException");
     } catch (IllegalArgumentException ex) {
-      assertThat(ex.getMessage(), is("Elements to populate random list from must not be empty"));
+      assertThat(ex.getMessage(), is("Elements to populate from must not be empty"));
     }
   }
 
@@ -78,7 +162,7 @@ public class RandomCollectionUtilsTest {
       randomListFrom(Collections.emptyList(), Range.singleton(2));
       fail("Should have thrown an IllegalArgumentException");
     } catch (IllegalArgumentException ex) {
-      assertThat(ex.getMessage(), is("Elements to populate random list from must not be empty"));
+      assertThat(ex.getMessage(), is("Elements to populate from must not be empty"));
     }
   }
 
@@ -86,7 +170,7 @@ public class RandomCollectionUtilsTest {
   public void randomListFrom_WithSingletonCollection_ReturnsListFilledWithOnlyElement() {
     Object onlyElement = new Object();
     List<Object> list = randomListFrom(Collections.singleton(onlyElement), Range.singleton(2));
-    assertThat(list, is(Lists.newArrayList(onlyElement, onlyElement)));
+    assertThat(list, contains(onlyElement, onlyElement));
   }
 
   @Test
